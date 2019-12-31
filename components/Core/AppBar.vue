aef71563a95ef9795e2d663fdb3b4368af86bd49
Henrik Wassdahl <henriq88@gmail.com>
2019-12-31 17:36:29 +0100

    ➕ update link indicator position on scroll

--- components/Core/AppBar.vue
+++ components/Core/AppBar.vue
@@ -1,12 +1,20 @@
 <template>
   <v-app-bar
+    v-scroll="updateLinkIndicator"
     color="transparent"
     app
     fixed
     :height="96"
   >
-    <v-row v-ripple class="shrink py-3 px-6 clickable" no-gutters align="center" style="height: inherit; margin: -4px 0 -4px -16px;">
-      <v-avatar color="grey darken-3">
+    <v-row
+      v-ripple
+      class="shrink py-3 px-6 clickable"
+      no-gutters
+      align="center"
+      style="height: inherit; margin: -4px 0 -4px -16px;"
+      @click="goTo(0)"
+    >
+      <v-avatar color="grey-darken-3">
         <v-icon large>
           mdi-account
         </v-icon>
@@ -17,8 +25,8 @@
       </v-row>
     </v-row>
     <v-spacer />
-    <v-tabs background-color="transparent" class="shrink" style="width: inherit;" color="white">
-      <v-tab class="white--text" style="opacity: inherit;" @click="goTo(`#portfolio`)">
+    <v-tabs v-model="linkIndex" background-color="transparent" class="shrink" style="width: inherit;" color="white">
+      <v-tab class="white--text" style="opacity: inherit;" @click.prevent="goTo(`#portfolio`)">
         Portfolio
       </v-tab>
       <v-tab class="white--text" style="opacity: inherit;" @click="goTo(`#about`)">
@@ -32,17 +40,35 @@
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+// import Vue, { VueConstructor } from 'vue'
+import mixins from 'vue-typed-mixins'
 
-export default Vue.extend({
+export default mixins().extend({
+  data: () => ({
+    linkIndex: 0,
+    linkIndicatorUpdateDisabled: false
+  }),
   methods: {
-    goTo (selector: string) {
+    async goTo (selector: string | number) {
+      const duration = 300
       const options = {
-        duration: 300,
+        duration,
         offset: 0,
         easing: 'easeInOutCubic'
       }
+      this.linkIndicatorUpdateDisabled = true
+      if (typeof selector === 'number') { this.linkIndex = 0 }
       this.$vuetify.goTo(selector, options)
+      await new Promise(resolve => setTimeout(resolve, duration * 1.1))
+      this.linkIndicatorUpdateDisabled = false
+    },
+    updateLinkIndicator (event: Event) {
+      if (this.linkIndicatorUpdateDisabled) { return }
+      const scrollPosition = document.documentElement.scrollTop || document.body.scrollTop
+      this.linkIndex = this.getLinkIndex(scrollPosition)
+    },
+    getLinkIndex (scrollPosition: number): number {
+      if (scrollPosition <= 170) { return 0 } else if (scrollPosition <= 800) { return 1 } else { return 2 }
     }
   }
 })
