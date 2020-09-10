dbdb3e4de2a48f5b8f53130d4834686564e4de21
Henrik Wassdahl <henriq88@gmail.com>
2020-09-11 00:16:41 +0900

    🔨 prod env

--- hosting/src/plugins/firebase/index.ts
+++ hosting/src/plugins/firebase/index.ts
@@ -5,7 +5,6 @@ import 'firebase/firestore'
 import 'firebase/performance'
 
 if (!firebase.apps.length) {
-  console.log(process.env.FIREBASE_PROJECT_ID)
   const firebaseConfig = {
     apiKey: process.env.FIREBASE_API_KEY,
     authDomain: process.env.FIREBASE_AUTH_DOMAIN,
