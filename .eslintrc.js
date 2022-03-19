ebd8c014e51fbac8bddfa58c849f60076713b927
Henrik Wassdahl <henriq88@gmail.com>
2022-03-20 02:37:45 +0900

    fix: next eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,3 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-    'airbnb/hooks',
-    'airbnb-typescript',
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    project: './tsconfig.json',
-  },
-  plugins: [
-    'react',
-    '@typescript-eslint',
-  ],
-  rules: {
-  },
-};
+  extends: "next/core-web-vitals"
+}
