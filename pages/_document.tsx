2e52cd7aac739a58625fa9fa30820bea22bcc584
Henrik Wassdahl <henriq88@gmail.com>
2022-03-20 05:51:30 +0900

    fix: meta tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,7 +8,7 @@ const Document: React.VFC = (props) => {
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;700&display=swap" rel="stylesheet" />
         <meta charSet="UTF-8" />
-        <meta charSet="Henrik Wassdahl - UX Developer" />
+        <meta name="description" content="Henrik Wassdahl - UX Developer" />
       </Head>
       <body dir="ltr">
         <Main />
