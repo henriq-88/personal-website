6eedfb30bba4aab3a688c33e57b61fea6a96b083
Henrik Wassdahl <henriq88@gmail.com>
2022-03-21 00:15:42 +0900

    feat: favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,14 @@ import AboutSection from "@/components/Section/About";
 import ContactSection from "@/components/Section/Contact";
 import IntroSection from "@/components/Section/Intro";
 import ProjectsSection from "@/components/Section/Projects";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Henrik Wassdahl - UX Developer</title>
+      </Head>
       <IntroSection />
       <AboutSection />
       <ProjectsSection />
