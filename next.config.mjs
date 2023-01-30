fa0e7181519d228ca1933f663b8a1674c4115323
Henrik Wassdahl <henriq88@gmail.com>
2023-01-31 03:25:57 +0900

    build!: remove ssr

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,9 @@ const nextConfig = {
   reactStrictMode: true,
   eslint: { ignoreDuringBuilds: !!process.env.CI },
   typescript: { ignoreBuildErrors: !!process.env.CI },
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
