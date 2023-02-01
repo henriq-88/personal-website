39ac087f2fd6d46bcec579037b9aec47f6f75159
Henrik Wassdahl <henriq88@gmail.com>
2023-02-01 20:12:03 +0900

    refactor: replace mui with tailwind 3/

--- src/components/ThemeToggle/ThemeToggle.tsx
+++ src/components/ThemeToggle/ThemeToggle.tsx
@@ -1,9 +1,9 @@
 import { SunIcon, MoonIcon, } from "@heroicons/react/24/solid"
 import SettingsBrightnessIcon from "@/components/Icons/SettingsBrigthness"
-import { ToggleButton, ToggleButtonGroup, Typography } from '@mui/material';
 import { useMemo } from 'react';
 import { themeModeState } from '@/state/states';
 import { useAtom } from 'jotai';
+import clsx from "clsx";
 
 interface ThemeToggleProps {
 }
@@ -27,52 +27,44 @@ const ThemeToggle: React.FC<ThemeToggleProps> = (props) => {
 
   return (
     <>
-      <Typography
-        variant="caption"
-        sx={{
-          textTransform: 'uppercase',
-        }}
-      >
+      <span className="uppercase text-xs">
         Mode
-      </Typography>
-      <ToggleButtonGroup
-        value={mode}
-        color="secondary"
-        exclusive
-        onChange={(event, newMode: typeof mode) => handleModeChange(newMode)}
-        aria-label="theme mode"
-      >
-        <ToggleButton
-          value="light"
+      </span>
+      <div className={"inline-flex rounded-md shadow-sm"} role="group">
+        <button
+          className={clsx("h-12 flex flex-1 items-center justify-center px-4 py-2 text-sm uppercase font-medium text-primary-light dark:text-primary-dark border border-[#0000001f] bg-transparent dark:border-[#ffffff1f] ltr:rounded-l-lg rtl:border-t rtl:border-b rtl:rounded-r-lg", {
+            "bg-primary-light/10 dark:bg-primary-dark/20 hover:bg-primary-light/20 dark:hover:bg-primary-dark/30": mode === "light",
+            "dark:hover:bg-[#ffffff14] hover:bg-[#00000014]": mode !== "light",
+          })}
           aria-label="light"
-          sx={{
-            flex: 1,
-          }}
+          onClick={() => handleModeChange(`light`)}
         >
           <SunIcon className="h-6 w-6 text-current ltr:mr-1 rtl:ml-1" />
           <span>Light</span>
-        </ToggleButton>
-        <ToggleButton
-          value="system"
+        </button>
+        <button
+          className={clsx("h-12 flex flex-1 items-center justify-center px-4 py-2 text-sm uppercase font-medium text-primary-light dark:text-primary-dark border-[#0000001f] bg-transparent dark:border-[#ffffff1f] border-t border-b", {
+            "bg-primary-light/10 dark:bg-primary-dark/20 hover:bg-primary-light/20 dark:hover:bg-primary-dark/30": mode === "system",
+            "dark:hover:bg-[#ffffff14] hover:bg-[#00000014]": mode !== "system",
+          })}
           aria-label="system"
-          sx={{
-            flex: 1,
-          }}
+          onClick={() => handleModeChange(`system`)}
         >
           <SettingsBrightnessIcon className="h-6 w-6 text-current ltr:mr-1 rtl:ml-1" />
           <span>System</span>
-        </ToggleButton>
-        <ToggleButton
-          value="dark"
+        </button>
+        <button
+          className={clsx("h-12 flex flex-1 items-center justify-center px-4 py-2 text-sm uppercase font-medium text-primary-light dark:text-primary-dark border border-[#0000001f] bg-transparent dark:border-[#ffffff1f] ltr:rounded-r-lg rtl:border-t rtl:border-b rtl:rounded-l-lg", {
+            "bg-primary-light/10 dark:bg-primary-dark/20 hover:bg-primary-light/20 dark:hover:bg-primary-dark/30": mode === "dark",
+            "dark:hover:bg-[#ffffff14] hover:bg-[#00000014]": mode !== "dark",
+          })}
           aria-label="dark"
-          sx={{
-            flex: 1,
-          }}
+          onClick={() => handleModeChange("dark")}
         >
           <MoonIcon className="h-6 w-6 text-current ltr:mr-1 rtl:ml-1" />
           <span>Dark</span>
-        </ToggleButton>
-      </ToggleButtonGroup>
+        </button>
+      </div>
     </>
   );
 };
