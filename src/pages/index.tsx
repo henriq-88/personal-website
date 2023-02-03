38ed7dd401e0082589ba78ce2d70418d9691b229
Henrik Wassdahl <henriq88@gmail.com>
2023-02-03 18:00:13 +0900

    fix: contact initial bg color

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,9 @@
 import IntroSection from "@/components/Section/Intro";
+import AboutSection from "@/components/Section/About";
+import ProjectsSection from "@/components/Section/Projects";
+import ContactSection from "@/components/Section/Contact";
 import FullPageSection from "@/components/Section/FullPage";
-import dynamic from "next/dynamic";
 import Head from "next/head";
-
-const AboutSection = dynamic(() => import('@/components/Section/About'), {
-  loading: () => <div>Loading</div>,
-})
-const ProjectsSection = dynamic(() => import('@/components/Section/Projects'), {
-  loading: () => <div>Loading</div>,
-})
-const ContactSection = dynamic(() => import('@/components/Section/Contact'), {
-  loading: () => <div>Loading</div>,
-})
 
 export default function Home() {
   return (
