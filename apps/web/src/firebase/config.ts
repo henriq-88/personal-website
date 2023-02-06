5b907bf3b9162bbef4a72cb7d06faa02e755cb39
Henrik Wassdahl <henriq88@gmail.com>
2023-02-07 05:10:28 +0900

    feat: remove firebase captcha

--- apps/web/src/firebase/config.ts
+++ apps/web/src/firebase/config.ts
@@ -2,11 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getPerformance } from "firebase/performance";
 import { getFunctions } from "firebase/functions";
-import { initializeAppCheck, ReCaptchaV3Provider } from "firebase/app-check";
-import { env } from "../env/client.mjs";
 
-// const config = process.env.NODE_ENV === `development`
-//   ? {
 const config = {
   apiKey: "AIzaSyCRVnGwzpOe8H0Cd1wr_qgeoBrUvslUdqc",
   authDomain: "personal-website-8af4a.firebaseapp.com",
@@ -17,17 +13,10 @@ const config = {
   appId: "1:386526278864:web:8560de4c9bd6fb7a26dad5",
   measurementId: "G-KV60RZRV91",
 };
-// : await fetch('/__/firebase/init.json').then(async response => response.json());
 
 const app = initializeApp(config);
 
 if (typeof window !== "undefined") {
-  const appCheck = initializeAppCheck(app, {
-    provider: new ReCaptchaV3Provider(
-      env.NEXT_PUBLIC_FIREBASE_RECAPTCHA_SITE_KEY,
-    ),
-    isTokenAutoRefreshEnabled: true,
-  });
   if (process.env.NODE_ENV === `production`) {
     const perf = getPerformance(app);
     const analytics = getAnalytics(app);
