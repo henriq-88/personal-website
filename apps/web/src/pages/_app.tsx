665a5f80e59618c9694a8342584b48f297f52bea
Henrik Wassdahl <henriq88@gmail.com>
2023-02-12 23:16:30 +0900

    feat: only load needed fonts

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -12,6 +12,7 @@ const poppins = Poppins({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
   subsets: ["latin"],
   display: "swap",
+  preload: false,
 });
 
 export default function MyApp({ Component, pageProps }: AppProps) {
