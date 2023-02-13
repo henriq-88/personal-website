8c2fd2dd0368650eaafd0181c4a92e72d9b2d102
Henrik Wassdahl <henriq88@gmail.com>
2023-02-13 17:03:07 +0900

    feat: rounded borders on project card

--- apps/web/src/components/Projects/Card/Card.tsx
+++ apps/web/src/components/Projects/Card/Card.tsx
@@ -1,14 +1,20 @@
+import clsx from "clsx";
 import Image from "next/image";
 import Link from "next/link";
 import MeImage from "../../../assets/images/me.jpg";
 
-interface ProjectCardProps {}
+interface ProjectCardProps {
+  className?: string;
+}
 
 const ProjectCard: React.FC<ProjectCardProps> = (props) => {
   return (
     <Link
       href="#"
-      className="group relative flex h-64 w-full items-center justify-center transition-all duration-300 ease-in-out hover:z-10 hover:scale-105"
+      className={clsx(
+        "group relative flex h-64 w-full items-center justify-center transition-all duration-300 ease-in-out hover:z-10 hover:scale-105",
+        props.className,
+      )}
     >
       <Image
         alt="picture of me"
@@ -16,13 +22,31 @@ const ProjectCard: React.FC<ProjectCardProps> = (props) => {
         src={MeImage}
         style={{
           objectFit: `cover`,
+          borderRadius: `inherit`,
         }}
       />
-      <div className="pointer-events-none absolute flex h-full w-full flex-col justify-between">
-        <div className="rounded-xl bg-gradient-to-b from-black/80 to-black/0 p-3 text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+      <div
+        className="pointer-events-none absolute flex h-full w-full flex-col justify-between"
+        style={{
+          borderRadius: `inherit`,
+        }}
+      >
+        <div
+          className="rounded-xl bg-gradient-to-b from-black/80 to-black/0 p-3 text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100"
+          style={{
+            borderTopRightRadius: `inherit`,
+            borderTopLeftRadius: `inherit`,
+          }}
+        >
           AOA - 사뿐사뿐 (Like a Cat)
         </div>
-        <div className="flex items-end justify-between rounded-xl bg-gradient-to-t from-black/80 to-black/0 p-3 text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+        <div
+          className="flex items-end justify-between rounded-xl bg-gradient-to-t from-black/80 to-black/0 p-3 text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100"
+          style={{
+            borderBottomRightRadius: `inherit`,
+            borderBottomLeftRadius: `inherit`,
+          }}
+        >
           <div>Nice</div>
           <div className="rounded-2xl bg-violet-900 px-3 py-1 text-xs">
             Model
--- apps/web/src/components/Projects/Projects.tsx
+++ apps/web/src/components/Projects/Projects.tsx
@@ -1,17 +1,63 @@
 import { Container, TextField } from "packages/ui";
 import ProjectCard from "./Card";
 import { useAutoAnimate } from "@formkit/auto-animate/react";
-import { useState } from "react";
+import { useCallback, useMemo, useState } from "react";
+import { useWindowSize } from "rooks";
 
 interface ProjectsSectionProps {}
 
 const ProjectsSection: React.FC<ProjectsSectionProps> = (props) => {
+  const { innerWidth } = useWindowSize();
   const [gridRef] = useAutoAnimate(/* optional config */);
 
   const [projects, setProjects] = useState([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
   const [search, setSearch] = useState("");
 
-  console.log({ search });
+  const columnCount = useMemo(() => {
+    if (!innerWidth) {
+      return 1;
+    }
+    if (innerWidth >= 1024) {
+      return 3;
+    }
+    if (innerWidth >= 640) {
+      return 2;
+    }
+    return 1;
+  }, [innerWidth]);
+
+  const getBorderRadiusClassName = useCallback(
+    ({
+      index,
+      length,
+      columnCount,
+    }: {
+      index: number;
+      length: number;
+      columnCount: number;
+    }) => {
+      return [
+        // top left
+        ...(index === 0 ? [`rounded-tl-xl`] : []),
+        // top right
+        ...(index % columnCount === columnCount - 1 && index < columnCount
+          ? [`rounded-tr-xl`]
+          : []),
+        // bottom left
+        ...(index % columnCount === 0 && length - columnCount <= index
+          ? [`rounded-bl-xl`]
+          : []),
+        // bottom right, second last row
+        ...(index % columnCount === columnCount - 1 &&
+        length - columnCount < index
+          ? [`rounded-br-xl`]
+          : []),
+        // bottom right, last row
+        ...(index === length - 1 ? [`rounded-br-xl`] : []),
+      ].join(` `);
+    },
+    [],
+  );
 
   return (
     <Container className="h-full flex-1">
@@ -26,9 +72,14 @@ const ProjectsSection: React.FC<ProjectsSectionProps> = (props) => {
         className="mt-3 grid grid-cols-1 gap-0.5 sm:grid-cols-2 lg:grid-cols-3"
         ref={gridRef}
       >
-        {projects.map((item) => (
-          <ProjectCard key={item} />
-        ))}
+        {projects.map((item, i, projects) => {
+          const borderRadiusClassName = getBorderRadiusClassName({
+            index: i,
+            length: projects.length,
+            columnCount,
+          });
+          return <ProjectCard key={item} className={borderRadiusClassName} />;
+        })}
       </div>
     </Container>
   );
