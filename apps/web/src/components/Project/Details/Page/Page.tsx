c10a1f3a81525cb7b1cf4e4d08647b5d283ce546
Henrik Wassdahl <henriq88@gmail.com>
2023-02-15 22:18:26 +0900

    fix: project details fade animation

--- apps/web/src/components/Project/Details/Page/Page.tsx
+++ apps/web/src/components/Project/Details/Page/Page.tsx
@@ -39,50 +39,48 @@ const ProjectDetailsPage: React.FC<ProjectDetailsPageProps> = (props) => {
             />
           </Fade>
         </div>
-        <Fade direction="left">
-          <Container className="relative flex w-full flex-col">
-            <div className="left-0 bg-violet-200 p-3 shadow-xl dark:bg-[#0C0417] sm:absolute sm:-top-20 sm:mx-3 sm:w-[calc(100%-1.5rem)] sm:rounded-xl sm:p-6">
-              <div className="flex flex-row items-start">
-                {projectData.logo && (
-                  <Image
-                    alt="project logo"
-                    src={projectData.logo}
-                    width={96}
-                    height={96}
-                    className="h-16 w-16 rounded-xl object-cover ltr:mr-6 rtl:ml-6 sm:h-24 sm:w-24"
-                  />
-                )}
-                <h1 className="text-3xl font-bold leading-tight sm:text-4xl md:text-6xl">
-                  {projectData.name}
-                </h1>
-              </div>
-              <div className="my-6 flex">
-                <CategoryChip category={projectData.category} size="medium" />
-              </div>
-              <div className="mt-3">
-                {projectData.website && (
-                  <Link
-                    href={projectData.website}
-                    target="_blank"
-                    className="inline-block text-gray-500 transition-transform ease-in-out hover:scale-105 hover:text-neutral-900 dark:text-gray-400 dark:hover:text-white"
-                  >
-                    <span className="break-words">{projectData.website}</span>
-                    <ArrowTopRightOnSquareIcon className="ml-2 mb-1 inline-block h-4 w-4" />
-                  </Link>
-                )}
-              </div>
-              <div className="my-6">
-                <Gallery medias={projectData.medias} />
-              </div>
-              <p
-                className="my-3 whitespace-pre-wrap"
-                dangerouslySetInnerHTML={{
-                  __html: projectData.body,
-                }}
-              />
+        <Container className="relative flex w-full flex-col">
+          <div className="left-0 bg-violet-200 p-3 shadow-xl dark:bg-[#0C0417] sm:absolute sm:-top-20 sm:mx-3 sm:w-[calc(100%-1.5rem)] sm:rounded-xl sm:p-6">
+            <div className="flex flex-row items-start">
+              {projectData.logo && (
+                <Image
+                  alt="project logo"
+                  src={projectData.logo}
+                  width={96}
+                  height={96}
+                  className="h-16 w-16 rounded-xl object-cover ltr:mr-6 rtl:ml-6 sm:h-24 sm:w-24"
+                />
+              )}
+              <h1 className="text-3xl font-bold leading-tight sm:text-4xl md:text-6xl">
+                {projectData.name}
+              </h1>
             </div>
-          </Container>
-        </Fade>
+            <div className="my-6 flex">
+              <CategoryChip category={projectData.category} size="medium" />
+            </div>
+            <div className="mt-3">
+              {projectData.website && (
+                <Link
+                  href={projectData.website}
+                  target="_blank"
+                  className="inline-block text-gray-500 transition-transform ease-in-out hover:scale-105 hover:text-neutral-900 dark:text-gray-400 dark:hover:text-white"
+                >
+                  <span className="break-words">{projectData.website}</span>
+                  <ArrowTopRightOnSquareIcon className="ml-2 mb-1 inline-block h-4 w-4" />
+                </Link>
+              )}
+            </div>
+            <div className="my-6">
+              <Gallery medias={projectData.medias} />
+            </div>
+            <p
+              className="my-3 whitespace-pre-wrap"
+              dangerouslySetInnerHTML={{
+                __html: projectData.body,
+              }}
+            />
+          </div>
+        </Container>
       </div>
     </>
   );
