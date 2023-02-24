0eda85f67d3fac833495c18d9552bb060d0ddd72
Henrik Wassdahl <henriq88@gmail.com>
2023-02-24 21:24:31 +0900

    feat: aria label on filter button

--- apps/web/src/components/Project/Filter/ToggleShowSectionButton/ToggleShowSectionButton.tsx
+++ apps/web/src/components/Project/Filter/ToggleShowSectionButton/ToggleShowSectionButton.tsx
@@ -19,6 +19,7 @@ const ToggleShowSectionButton: React.FC<ToggleShowSectionButtonProps> = (
         "flex shrink flex-row items-center rounded-xl border border-violet-500 p-3 transition-all hover:border-violet-900 hover:bg-violet-900/10 dark:border-violet-900 hover:dark:border-violet-500 hover:dark:bg-violet-500/10",
         className,
       )}
+      aria-label={isOpen ? `Hide filters` : `Show filters`}
       {...rest}
     >
       <FunnelIcon className="h-6 w-6 text-violet-900 dark:text-violet-500" />
